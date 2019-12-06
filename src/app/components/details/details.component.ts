46e52a4f343420b84287afb12ca331a133ca4753
Jacopo <c.tejada@beanar.io>
2019-12-05 23:41:29 -0600

    camnio en el search bar movie

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
-import { PeliculaDetalle, RespuestaCredits } from '../../interfaces/interface';
+import { PeliculaDetalle, RespuestaCredits, Cast } from '../../interfaces/interface';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
@@ -11,20 +12,36 @@ export class DetailsComponent implements OnInit {
   @Input() id;
   pelicula: PeliculaDetalle = {};
   credits: RespuestaCredits;
-  constructor(private moviesService:MoviesService) { }
+  ocularChar = 150;
+  actores: Cast[] = [];
+  optionSLides = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5,
+  };
+  constructor(
+    private moviesService:MoviesService,
+    private modalController:ModalController) { }
 
   ngOnInit() {
     
     this.moviesService.getMovieDetails(this.id)
         .subscribe(resp => {
               console.log('DESTAILS', resp);
-              this.pelicula=resp;
+              this.pelicula = resp;
         });
     this.moviesService.getMovieCredits(this.id)
         .subscribe(resp => {
               console.log('CREDITS', resp);
               this.credits = resp;
+              this.actores = resp.cast;
         });
   }
-
+  regresar() {
+    console.log('texto');
+    this.modalController.dismiss();
+  }
+  favoridoAdd(){
+    console.log('ADD FAV,comign soon');
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  // @ViewChild('search') search:IonSearchbar;
+  textoBuscar = '';
+  ideas: string[]= ['Spiderman', 'Scary Movie', 'Mia Khalifa', 'Avengers'];
   constructor() {}
+
+  onSearchChange( event ) {
+    console.log(event.target.value);
+  }
+
+  // setIdea(event){
+  //  console.log(event.target.textContent);
+  // //  this.search.value = event.target.textContent;
+  // }
 
 }
